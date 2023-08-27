3a065df4aa5cfeee04fd23023adae532d28ecbb5
BuildTools <unconfigured@null.spigotmc.org>
2023-08-27 15:26:48 -0700

    functional rotational cube in 3D!

--- Main.java
+++ Main.java
@@ -4,83 +4,51 @@ import java.util.Arrays;
 import javax.swing.*;
 public class Main extends JFrame {
 	
-	public static double resx = 1920, resy = 1080, rotationDelay = 10, angle = 0;
-	public static Point[] points = new Point[4];
+	public static double resx = 1920, resy = 1080, rotationDelay = 1;
+	public static Point[] points = new Point[8];
 	
-	public static double[][] conversionMatrix =  {
-			{1,0,0},
-			{0,1,0}
-		};
-	
-	public static double[][] rotationMatrix = {
-			{Math.cos(angle),-Math.sin(angle),0},
-			{Math.sin(angle), Math.cos(angle), 0},
-			{0,0,1}
-	};
 	public Main() {
 		
-		points[0] = new Point(100,100,0);
-  		points[1] = new Point(100,-100,0);
-  		points[2] = new Point(-100,100,0);
-  		points[3] = new Point(-100,-100,0);
-  		
-		for(Point p:points) {
-			double[][] toMatrix = {
-					{p.x},
-					{p.y},
-					{p.z}
-			};
-			double[][] result = matrixMult(conversionMatrix,toMatrix);
-			p.threedx=result[0][0];
-			p.threedy=result[1][0];
-			//System.out.println(matrixMult(conversionMatrix,toMatrix));
-		}
-  		
+		//Points to make a cube
+		points[0] = new Point(100,100,100);
+  		points[1] = new Point(100,-100,100);
+  		points[2] = new Point(-100,100,100);
+  		points[3] = new Point(-100,-100,100);
+  		points[4] = new Point(100,100,-100);
+  		points[5] = new Point(100,-100,-100);
+  		points[6] = new Point(-100,100,-100);
+  		points[7] = new Point(-100,-100,-100);
   		
 		setTitle("3D Projection");
 		setSize((int)resx,(int)resy);
 		setLayout(new BorderLayout());
+		setDefaultCloseOperation(this.EXIT_ON_CLOSE);
 		final PaintPanel paintPan = new PaintPanel();
 		final JPanel panel = new JPanel();
 		add(panel, BorderLayout.EAST);
 		add(paintPan, BorderLayout.CENTER);
 		setVisible(true);
 		
+		double angle = 0;
+		
 		while(true) {
-			angle+=1;
+			// timer = new Timer(10,e->updateGraphics(angle));
 			try {
 				Thread.sleep((long)rotationDelay);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			paintPan.updateGraphics();
+			angle+=0.005;
+			paintPan.updateGraphics(angle);
 		}
 	}
-	// Created only for this scenario, as a result, only generates the 2 points of x and y
-	public static double[][] matrixMult(double[][] m1, double[][] m2) {
-		double[][] result;
-		if(m1[0].length!=m2.length) {
-			System.out.println("ERR: Invalid Matrix Dimensions");
-			return null;
-		} else {
-			result = new double[m1.length][m2[0].length];
-			//result = new int[20][20];
-			//System.out.println(m1.length+"x"+m2[0].length);
-			for(int k=0;k<m2[0].length;k++) {
-				for(int j=0;j<m1.length;j++) {
-					int sum = 0;
-					for(int i=0;i<m1[0].length;i++) {
-					//	System.out.println(j+","+i+";"+k+","+i);
-						sum+=m1[j][i]*m2[i][k];
-					}
-					//System.out.println(sum);
-					result[j][k]=sum;
-				}
-			}
-		}
-		return result;
+	private Object updateGraphics(double angle) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+	// Multiplies a mxn by nxr matrix provided mxr
+	
 	public static void main(String[] args) {
 		new Main();
 	}
@@ -100,13 +68,12 @@ class PaintPanel extends JPanel {
 		Graphics2D drawImage = (Graphics2D) g;
 		for(Point p:Main.points) {
 			drawImage.setColor(Color.GREEN);
-			drawImage.fillOval((int)p.threedx/2+1920/2,(int)p.threedy/2+1080/2,10,10);
+			drawImage.fillOval((int)p.twoDx/2+1920/3,(int)p.twoDy/2+1080/3,10,10);
 		}
 	}
-	public void updateGraphics() {
+	public void updateGraphics(double angle) {
 		for(Point p:Main.points) {
-			p.threedx+=1;
-			p.threedy+=1;
+			p.updatePoint(angle);
 		}
 		repaint();
 	}
--- Point.java
+++ Point.java
@@ -4,12 +4,103 @@ public class Point {
 	public int y;
 	public int z;
 	
-	public double threedx;
-	public double threedy;
+	public double twoDx;
+	public double twoDy;
+	
+	public static double[][] conversionMatrix =  {
+			{1,0,0},
+			{0,1,0}
+		};
 	
 	public Point(int x_, int y_, int z_) {
 		x=x_;
 		y=y_;
 		z=z_;
+		double[][] toMatrix = {
+				{x},
+				{y},
+				{z}
+		};
+		double[][] result = matrixMult(conversionMatrix,toMatrix);
+		twoDx=result[0][0];
+		twoDy=result[1][0];
+	}
+	
+	public void updatePoint(double angle) {
+		double[][] toMatrix = {
+				{x},
+				{y},
+				{z}
+		};
+		/*
+		 * Rotation matrix help from https://www.youtube.com/watch?v=p4Iz0XJY-Qk
+		 */
+		double[][] rotationMatrixX = {
+				{1,0,0},
+				{0,Math.cos(angle),-Math.sin(angle)},
+				{0,Math.sin(angle),Math.cos(angle)}
+		};
+		double[][] rotationMatrixY = {
+				{Math.cos(angle),0,Math.sin(angle)},
+				{0,1,0},
+				{-Math.sin(angle),0,Math.cos(angle)}
+		};
+		double[][] rotationMatrixZ = {
+				{Math.cos(angle),-Math.sin(angle),0},
+				{Math.sin(angle), Math.cos(angle), 0},
+				{0,0,1}
+		};
+		double[][] rotate = matrixMult(rotationMatrixY,toMatrix);
+		rotate = matrixMult(rotationMatrixX,rotate);
+		rotate = matrixMult(rotationMatrixZ,rotate);
+		double[][] result = matrixMult(conversionMatrix,rotate);
+		//double[][] result = matrixMult(conversionMatrix,toMatrix);
+		twoDx=result[0][0];
+		twoDy=result[1][0];
+	}
+	
+	/*
+	//TESTCASE FOR MATRIX MULTIPLY;;WORKING
+		double[][] matrixA = {
+				{-2, 2, -2},
+				{0, 4, 1},
+				{1, 1, 3}
+		};
+		double[][] matrixB = {
+				{0, -1, 1},
+				{4, -3, -2},
+				{3, 2, 1}
+		};
+		
+		double[][] result = matrixMult(matrixA, matrixB);
+		for(int i=0;i<result.length;i++) {
+			for(int j=0;j<result[0].length;j++) {
+				System.out.print(result[i][j]+";");
+			}
+			System.out.println();
+		}*/
+	
+	public static double[][] matrixMult(double[][] m1, double[][] m2) {
+		double[][] result;
+		if(m1[0].length!=m2.length) {
+			System.out.println("ERR: Invalid Matrix Dimensions");
+			return null;
+		} else {
+			result = new double[m1.length][m2[0].length];
+			//result = new int[20][20];
+			//System.out.println(m1.length+"x"+m2[0].length);
+			for(int k=0;k<m2[0].length;k++) {
+				for(int j=0;j<m1.length;j++) {
+					int sum = 0;
+					for(int i=0;i<m1[0].length;i++) {
+					//	System.out.println(j+","+i+";"+k+","+i);
+						sum+=m1[j][i]*m2[i][k];
+					}
+					//System.out.println(sum);
+					result[j][k]=sum;
+				}
+			}
+		}
+		return result;
 	}
 }
